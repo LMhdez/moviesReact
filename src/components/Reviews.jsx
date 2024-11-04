6689f6b280cefe85f06be51f26b37a0271e3bc7b
lhernandez <luismariohernandezsantos@gmail.com>
2024-11-04 09:41:41 +0000

    margin in reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -2,20 +2,23 @@ import useFetch from "../hooks/useFetch";
 import ReviewsCard from "./ReviewsCard";
 
 const Reviews = ({ reviews }) => {
-	return (
-		<>
-			<div>
-				<h2 className="font-black ml-8">Last Reviews</h2>
-				<ul className="flex flex-row flex-wrap justify-center sm:justify-start items-start">
-					{reviews.map((review, index) => (
-						<li key={index} className="w-1/2 min-w-72">
-							{review && <ReviewsCard review={review} />}
-						</li>
-					))}
-				</ul>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="mx-8 mt-8">
+        <h2 className="font-black ">Last Reviews</h2>
+        {reviews && reviews.length > 0 && (
+          <ul className="flex flex-row flex-wrap justify-center sm:justify-start items-start">
+            {reviews.map((review, index) => (
+              <li key={index} className="w-1/2 min-w-72">
+                {review && <ReviewsCard review={review} />}
+              </li>
+            ))}
+          </ul>
+        )}
+        {reviews && reviews.length === 0 && <p>No reviews available.</p>}
+      </div>
+    </>
+  );
 };
 
 export default Reviews;
